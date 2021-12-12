4aa70b5e9c753de922f58c42289ccbbfda3a3482
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2021-12-12 14:45:36 +0100

    :wrench: Set up Apollo Server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,45 @@
 require('dotenv').config({ path: '.env' })
 
-const express = require('express')
-const app = express()
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import express from 'express'
+import http from 'http'
+import typeDefs from './type-defs'
+import resolvers from './resolvers'
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+async function startServer(typeDefs, resolvers) {
+	const app = express()
+	const httpServer = http.createServer(app)
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+	})
 
-// METRICS
+	// METRICS
 
-// CORS
+	// CORS
 
-// PRISMA
+	// PRISMA
 
-// BODY PARSER
+	// BODY PARSER
 
-// APOLLO SERVER
+	app.get('/hello', (req, res) => {
+		res.send('Hello World!')
+	})
 
-// Populate user
+	// Populate user
 
-// Health check
+	// Health check
 
-app.listen(process.env.PORT, () => {
-	console.log(`Example app listening at http://localhost:${process.env.PORT}`)
-})
+	await server.start()
+
+	// mount Apollo middleware
+	server.applyMiddleware({ app })
+
+	await new Promise<void>(resolve => httpServer.listen({ port: process.env.PORT }, resolve))
+
+	console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
+}
+
+startServer(typeDefs, resolvers)
