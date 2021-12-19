dd375cf7a8a14fae5cc1a2377eb1b2eadfc21b0c
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2021-12-19 21:40:35 +0100

    :sparkles: Collapsible table

--- frontend/src/components/Admin/UsersManagement.tsx
+++ frontend/src/components/Admin/UsersManagement.tsx
@@ -7,53 +7,69 @@ import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
-import { Accordion, AccordionDetails, AccordionSummary, Alert, Chip, Typography } from '@mui/material'
+import { Alert, Box, Chip, Collapse, IconButton, Typography } from '@mui/material'
 import Button from '@mui/material/Button'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
 import VerifiedUserIcon from '@mui/icons-material/VerifiedUser'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+
+const Row: React.FC<{ user: User }> = ({ user }) => {
+	const [open, setOpen] = React.useState(false)
+
+	return (
+		<React.Fragment>
+			<TableRow sx={{ '& > *': { borderBottom: 'unset' } }} onClick={() => setOpen(!open)}>
+				<TableCell scope="row">{user.name}</TableCell>
+				<TableCell>{user.email}</TableCell>
+				<TableCell align="right">
+					<IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+						{open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+					</IconButton>
+				</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={3}>
+					<Collapse in={open} timeout="auto" unmountOnExit>
+						<Box sx={{ margin: 2 }}>
+							<Typography variant="body1" sx={{ mb: 2 }} component="div">
+								Permissions
+							</Typography>
+							{user.permissions
+								?.filter(perm => perm && perm.name)
+								.map(perm => (
+									<Chip
+										color="primary"
+										icon={perm?.name === 'ADMIN' ? <VerifiedUserIcon /> : undefined}
+										label={perm?.name}
+										variant="outlined"
+										sx={{ ml: '1rem' }}
+									/>
+								))}
+						</Box>
+					</Collapse>
+				</TableCell>
+			</TableRow>
+		</React.Fragment>
+	)
+}
 
 export const UsersManagement: React.FC<{ users: UsersQuery['users'] }> = ({ users }) => {
 	if (!users) {
 		return <Alert severity="info">No users</Alert>
 	} else {
 		return (
-			<div>
-				<Typography variant="h6">
-					<div style={{ display: 'flex' }}>
-						<div style={{ flexBasis: '25%' }}>Email</div>
-						<div style={{ flexGrow: '1' }}>Name</div>
-					</div>
-				</Typography>
-				{users.map(
-					user =>
-						!!user && (
-							<Accordion>
-								<AccordionSummary
-									expandIcon={<ExpandMoreIcon />}
-									aria-controls="panel1a-content"
-									id="panel1a-header"
-									sx={{ display: 'flex', flexWrap: 'wrap' }}>
-									<div style={{ flexBasis: '25%' }}>{user.email}</div>
-									<div style={{ flexGrow: '1' }}>{user.name}</div>
-								</AccordionSummary>
-								<AccordionDetails>
-									{user.permissions
-										?.filter(perm => perm && perm.name)
-										.map(perm => (
-											<Chip
-												icon={perm?.name === 'ADMIN' ? <VerifiedUserIcon /> : undefined}
-												label={perm?.name}
-												variant="outlined"
-												sx={{ ml: '1rem' }}
-											/>
-										))}
-								</AccordionDetails>
-							</Accordion>
-						)
-				)}
-			</div>
+			<TableContainer component={Paper}>
+				<Table aria-label="collapsible table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Name</TableCell>
+							<TableCell>Email</TableCell>
+							<TableCell />
+						</TableRow>
+					</TableHead>
+					<TableBody>{users?.map(user => user && <Row key={user.id} user={user as User} />)}</TableBody>
+				</Table>
+			</TableContainer>
 		)
 	}
 }
