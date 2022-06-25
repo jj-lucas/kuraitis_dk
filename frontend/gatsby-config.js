9156e7de114307c9bf48101faa08691787fcbb39
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2022-06-25 12:07:06 +0200

    :sparkles: Make Footer text dynamic

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -9,6 +9,91 @@ module.exports = {
 	siteMetadata: {
 		siteUrl: 'https://www.yourdomain.tld',
 		title: 'Gatsby typescript',
+		mocks: {
+			product: {
+				code: 'OR',
+				current_variation: 'TAN',
+				other_variations: [
+					{
+						url: '/brown',
+						image: 'https://res.cloudinary.com/kuraitis/image/upload/v1641724594/products/l3xco11gwlqk71oe8nal.jpg',
+					},
+					{
+						url: '/black',
+						image: 'https://res.cloudinary.com/kuraitis/image/upload/v1641727554/products/khyuhmmmaxhzmepk7cws.jpg',
+					},
+				],
+				sizes: ['S', 'L'],
+				customizable: true,
+				breadcrumbs: [
+					{
+						label: 'Lorem',
+						url: '/lorem',
+					},
+					{
+						label: 'Ipsum',
+						url: '/ipsum',
+					},
+				],
+				title: 'Lorem ipsum dolor',
+				long_title: 'Lorem ipsum dolor sit amet consectetur',
+				description:
+					'If you are always looking for your things and you appreciate practical storage solutions, this leather tray will allow you to store your belongings in a beautiful natural style. Your keys, coins, pens and everything your imagination determines can be contained in this practical leather tray that will delight your home or office. It is of robust construction but at the same time, soft to the touch and with a fragrance and colors characteristic of natural leather.\n\nIf you are always looking for your things and you appreciate practical storage solutions, this leather tray will allow you to store your belongings in a beautiful natural style. Your keys, coins, pens and everything your imagination determines can be contained in this practical leather tray that will delight your home or office. It is of robust construction but at the same time, soft to the touch and with a fragrance and colors characteristic of natural leather.',
+				material: ['leather'],
+				details: ['Lorem ipsum dolor sit amet 1', 'Lorem ipsum dolor sit amet 2', 'Lorem ipsum dolor sit amet 3'],
+				prices: {
+					S: {
+						dkk: 43000,
+						usd: 7800,
+					},
+					L: {
+						dkk: 43000,
+						usd: 7800,
+					},
+				},
+				size_chart: 'http://www.google.com',
+				images: {
+					product: [
+						'https://res.cloudinary.com/kuraitis/image/upload/v1641665283/products/ouvgk0hsjnsvrozjqdxk.jpg',
+						'https://res.cloudinary.com/kuraitis/image/upload/v1617901813/products/to8omylzrz4hmg6mwhn5.jpg',
+						'https://res.cloudinary.com/kuraitis/image/upload/v1617901792/products/wokipe2phwvvuqois6rt.jpg',
+					],
+					parallax:
+						'https://previews.123rf.com/images/mtoome/mtoome1609/mtoome160900099/63492873-leather-handbag-craftsman-at-work-in-a-workshop.jpg',
+				},
+			},
+			translations: {
+				product_details_sizing: 'Sizing info text. Potentially a small size chart.',
+				product_details_shipping_returns: [
+					'Lorem ipsum dolor sit amet 1',
+					'Lorem ipsum dolor sit amet 2',
+					'Lorem ipsum dolor sit amet 3',
+					'Lorem ipsum dolor sit amet 4',
+					'Lorem ipsum dolor sit amet 5',
+				],
+				product_details_shipping_returns_customization: 'Special paragraph about customization',
+				product_materials: {
+					leather:
+						'An interesting description about the materials being used based on the selected materials from the admin. Selling points, why sustainability, etc. An interesting description about the materials being used based on the selected materials from the admin. Selling points, why sustainability, etc.',
+				},
+				product_customization:
+					"We can add up to three initials to each item which can be embossed by pressure onto the surface of the leather, adding a unique touch of individuality and personality. Request such customization in the 'customization' field above.",
+				product_selling_points: {
+					shipping: '1-3 days until your order is made and shipped.',
+					gift: 'All items are shipped in gift packaging.',
+					cancellations_returns:
+						'Free cancellations prior to shipping.\n30 days return right for non-customized products',
+					product_sergio:
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+				},
+				footer:
+					'© 2022, Sergio Kuraitis - Naturligt Design. CVR: 32220649 Højrisvej 13, 7900 Nykøbing Mors, Denmark | sergio@kuraitis.dk',
+				footer_privatlivs: 'Privatlivspolitik',
+				footer_cookie: 'Cookiepolitik',
+			},
+			review:
+				'I love this key organizer. Great craftsmanship & shipped pretty quickly. I would definitely purchase from Sergio again.',
+		},
 	},
 	plugins: [
 		{
@@ -35,7 +120,7 @@ module.exports = {
 		'gatsby-transformer-remark',
 		'gatsby-plugin-sharp',
 		'gatsby-transformer-sharp',
-		// `gatsby-plugin-graphql-codegen`,
+		`gatsby-plugin-graphql-codegen`,
 		{
 			resolve: 'gatsby-plugin-apollo',
 			options: {
--- frontend/src/components/Site/Footer.tsx
+++ frontend/src/components/Site/Footer.tsx
@@ -2,28 +2,25 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { AiFillStar as StarIcon } from 'react-icons/ai'
 import { Wrapper } from '@/components'
+import { useStaticQuery, graphql } from 'gatsby'
+import { FooterQuery } from '../../../graphql-types'
 
-const Reviews: React.FC = () => {
-	const [review, setReview] = useState('')
+const Review: React.FC = () => {
+	const site = {
+		siteMetadata: { mocks: { review: 'test' } },
+	}
 
-	useEffect(() => {
-		// get review from API
-		setReview(
-			'I love this key organizer. Great craftsmanship & shipped pretty quickly. I would definitely purchase from Sergio again.'
-		)
-	})
-
-	if (!review) return null
+	if (!site) return null
 
 	return (
-		<p className="reviews">
+		<p className="review">
 			<StarIcon />
 			<StarIcon />
 			<StarIcon />
 			<StarIcon />
 			<StarIcon />
 			<br />
-			{review}
+			{site.siteMetadata?.mocks?.review}
 			<br />
 			<a href="#">Læs mere feedback</a>
 		</p>
@@ -38,21 +35,37 @@ const StyledFooter = styled.div`
 	}
 `
 
-const Footer: React.FC = () => (
-	<Wrapper>
-		<StyledFooter>
-			<Reviews />
-			<div className="address">
-				© 2022, Sergio Kuraitis - Naturligt Design. CVR: 32220649 Højrisvej 13, 7900 Nykøbing Mors, Denmark |
-				sergio@kuraitis.dk
-			</div>
-			<div className="links">
-				<a href="#">Privatlivspolitik</a>
-				{' | '}
-				<a href="#">Cookiepolitik</a>
-			</div>
-		</StyledFooter>
-	</Wrapper>
-)
+const Footer: React.FC = () => {
+	const { site } = useStaticQuery<FooterQuery>(graphql`
+		query Footer {
+			site {
+				siteMetadata {
+					mocks {
+						review
+						translations {
+							footer
+							footer_cookie
+							footer_privatlivs
+						}
+					}
+				}
+			}
+		}
+	`)
+
+	return (
+		<Wrapper>
+			<StyledFooter>
+				<Review />
+				<div className="address">{site?.siteMetadata?.mocks?.translations?.footer}</div>
+				<div className="links">
+					<a href="#">{site?.siteMetadata?.mocks?.translations?.footer_privatlivs}</a>
+					{' | '}
+					<a href="#">{site?.siteMetadata?.mocks?.translations?.footer_cookie}</a>
+				</div>
+			</StyledFooter>
+		</Wrapper>
+	)
+}
 
 export { Footer }
